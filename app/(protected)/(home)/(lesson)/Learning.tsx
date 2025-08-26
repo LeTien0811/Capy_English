1b0b413542ebc42fa31dbbab43cb116ff6d0f9f8
LeVietTien <letien2081@gmail.com>
2025-08-26 21:46:52 +0700

    Update Lesson and learning

--- app/(protected)/(home)/(lesson)/Learning.tsx
+++ app/(protected)/(home)/(lesson)/Learning.tsx
@@ -1,24 +1,87 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute } from "@react-navigation/native";
+import LearningLessionCart from "@/components/LearningLessionCart";
+
+type LessonContext = {
+  QuestionID: string;
+  Question: string;
+  Answer_A: string;
+  Answer_B: string;
+  Answer_C: string;
+  correctAnswer: string;
+};
+
+type UserLessonContext = {
+  QuestionID: string;
+  Question: string;
+  Answer_A: string;
+  Answer_B: string;
+  Answer_C: string;
+  correctAnswer: string;
+  SelectANswer: string;
+};
 
 export default function Learning() {
-  const {lesson} = useLocalSearchParams();
-  let parsedLesson = [];
+  const router = useRouter();
+  const [isCurrentQuestion, setIsCurrentQuestion] = useState(0);
+  const [isUserSelectAnswer, setIsUserSelectAnswer] = useState<UserLessonContext[]>([]);
+  const [isScore, setIsScore] = useState(0);
+  const [isQuestion, setIsQuestion] = useState<any[]>([]);
+  const { lesson } = useLocalSearchParams();
 
-  if (typeof lesson === 'string') {
-    try {
-      parsedLesson = JSON.parse(lesson);
-    } catch (e) {
-      console.error("Invalid JSON in lesson param", e);
+  useEffect(() => {
+    if (typeof lesson === "string") {
+      try {
+        const parsedLesson: LessonContext[] = JSON.parse(lesson);
+        if (parsedLesson.length > 0) {
+          setIsQuestion(parsedLesson);
+        } else {
+          router.back();
+        }
+      } catch (e) {
+        console.log("Invalid JSON in lesson param", e);
+      }
+    } else {
+      console.log("lesson param is not a string", lesson);
     }
-  } else {
-    console.warn("lesson param is not a string", lesson);
+  }, [lesson]);
+
+  const isQuestionLength = isQuestion.length;
+
+  const handleAnswer = (selected: string) => {
+    if (isQuestion[isCurrentQuestion] === selected) {
+      setIsScore(prev => prev + (isQuestionLength / 10));
+    } else {
+      const NewUserAnswer: UserLessonContext = {
+        QuestionID: isQuestion[isCurrentQuestion].QuestionID,
+        Question: isQuestion[isCurrentQuestion].Question,
+        Answer_A: isQuestion[isCurrentQuestion].Answer_A,
+        Answer_B: isQuestion[isCurrentQuestion].Answer_A,
+        Answer_C: isQuestion[isCurrentQuestion].Answer_A,
+        correctAnswer: isQuestion[isCurrentQuestion].Answer_A,
+        SelectANswer: selected
+      };
+      setIsUserSelectAnswer(prev => [...prev, NewUserAnswer]);
+    }
   }
-  alert(parsedLesson)
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+
+  if (isQuestionLength > 0) {
+    return (
+      <SafeAreaView className="w-full h-full">
+        <View className="w-full h-full items-center flex justify-center">
+          {isQuestionLength > 0 && (
+            <LearningLessionCart
+              Lesson={isQuestion[isCurrentQuestion]}
+              onAnswer={handleAnswer}
+            />
+          )}
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return null;
+  }
 }
