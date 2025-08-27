fa33d9f9fce8e1efd0bb0c7eeb42c40ca3e15910
LeVietTien <letien2081@gmail.com>
2025-08-27 16:26:26 +0700

    Update Connect DATABASE

--- RestAPI/RestAPI/settings.py
+++ RestAPI/RestAPI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'RestAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "capystar",
+        'USER': "root",
+        'PASSWORD': "",
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
